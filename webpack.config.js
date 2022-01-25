2b5d568d9f0a9b630b5c7c503361dfef2a2af590
vincent trang <vincenttrang@vincents-MBP.lan>
2022-01-25 12:05:45 -0800

    1/25/22 12:05pm progress

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    plugins: [new HtmlWebpackPlugin ({
+        template: './src/index.html'
+    })]
+}
+
